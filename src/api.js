8b892c2666e6e0e43c8bf04e178f0e7f8999e6ec
SeisDani <danielayalaof@gmail.com>
2024-04-15 14:51:16 -0700

    0.1

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const searchImage = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client'
+    const url = 'https://api.unsplash.com/search/photos/?client_id=6tOYXzh2nzaWS'
+                = 'https://api.unsplash.com/search/photos/?client_id='
     const response = await axios.get(url, {
         params: {
             query: 'cars'
