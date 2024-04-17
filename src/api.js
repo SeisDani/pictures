9465901f3368d9923c5f875812d698afa87d3288
SeisDani <danielayalaof@gmail.com>
2024-04-17 14:39:50 -0700

    Agregue el componente SearchBar a App.jsx

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const searchImage = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id=6tOYXzh2nzaWSS2_6BL0Jmx-rBQDTWN2KrP2UugZYew'
-    
+    const url = 'https://api.unsplash.com/search/photos/?client_id='
+
     const response = await axios.get(url, {
         params: {
             query: 'cars'
