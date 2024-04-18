4c887615d8c59532653aa02ee94f28adf1094400
SeisDani <danielayalaof@gmail.com>
2024-04-18 16:24:36 -0700

    Actualizamos la api y pusimos datos en ImageList.jsx

--- src/api.js
+++ src/api.js
@@ -1,15 +1,18 @@
 import axios from 'axios'
 
 const searchImage = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id='
+    const url = 'https://api.unsplash.com/search/photos/?client_id=JihENSXoxkpcVvm84JZgUb-ud5njQwkF6m1iudJUZFY'
 
     const response = await axios.get(url, {
         params: {
-            query: 'cars'
+            query: term
         }
     })
 
-    console.log(response)
+    console.log(term)
+    console.log(response.data.results)
+
+    return response.data.results
 }
 
 export default searchImage
--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,7 +1,13 @@
-function ImageList(){
+import ImageShow from './ImageShow'
+
+function ImageList({ images}){
+    const renderImage = images.map((image) => {
+        return <ImageShow key={image.id} image={image}/>
+    })
+    
     return(
         <div>
-            <h1>Images: </h1>
+            {renderImage}
         </div>
     )
 }
